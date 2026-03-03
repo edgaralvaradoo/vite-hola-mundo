9447ddf3ec81ba99be9ca7efdd67a221c0d7b38d
Edgar Alvarado <al22020334@itsa,edu,mx>
2026-03-03 08:55:56 -0600

    cambio en el nombre de los contadores

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          El conteo es : {count}  
+          El contador marca : {count}  
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
